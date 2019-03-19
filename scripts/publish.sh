20a39d5403332759ac19383c18f1731eaff85afb
Jabari Dash <jabari.dash@gmail.com>
2019-03-19 01:11:43 -0500

    dont publish PRS

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,12 +4,16 @@ cd ${TRAVIS_BUILD_DIR}
 
 TAG="${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}"
 
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  exit 0
+fi
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build -t jabaridash/mazes:${TAG} .
 docker push jabaridash/mazes:${TAG}
 
-if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+if [ $TRAVIS_BRANCH == "master" ]; then
   docker tag jabaridash/mazes:${TAG} jabaridash/mazes
   docker push jabaridash/mazes
 fi
