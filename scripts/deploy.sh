07c6bca630c866501c41139898393c7072a98748
Jabari Dash <jabari.dash@gmail.com>
2019-03-19 01:02:55 -0500

    more issues with typos

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
 set -e
 
 cd ${TRAVIS_BUILD_DIR}
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
+if [ $TRAVIS_PULL_REQUEST == "false" ]; then
   firebase deploy --token $FIREBASE_TOKEN
 fi
