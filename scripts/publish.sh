3ecdd9cbfbeb0b90a2e254b78cb993d7130192cd
Jabari Dash <jabari.dash@gmail.com>
2019-03-19 01:15:01 -0500

    yeah, build PRS

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,7 @@ cd ${TRAVIS_BUILD_DIR}
 TAG="${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}"
 
 if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-  exit 0
+  TAG="PR-${TRAVIS_PULL_REQUEST}-${TRAVIS_BUILD_NUMBER}" 
 fi
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
