02ad31a6e23ad403d54e260f11be750cca3bbf5d
Jabari Dash <jabari.dash@gmail.com>
2019-03-19 01:20:55 -0500

    almost there

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,7 +13,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t jabaridash/mazes:${TAG} .
 docker push jabaridash/mazes:${TAG}
 
-if [ $TRAVIS_BRANCH == "master" ]; then
+if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
   docker tag jabaridash/mazes:${TAG} jabaridash/mazes
   docker push jabaridash/mazes
 fi
