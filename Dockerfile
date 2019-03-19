a02521fd1b8fd6e7430daf9d13f4e48d10d7de26
Jabari Dash <jabari.dash@gmail.com>
2019-03-18 22:51:08 -0500

    deploys as firebase hosting

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,6 @@
-##############
-# Build docs #
-##############
-FROM jekyll/jekyll as build-docs
-
-ADD ./ /mazes
-WORKDIR /mazes/docs
-#RUN bundle install && bundle exec jekyll build
-
-##############
-# Build page #
-##############
-FROM node:8.7 as build-page
-
-COPY --from=build-docs /mazes /mazes
-WORKDIR /mazes/src
-RUN npm install
-
-##############
-# Build prod #
-##############
 FROM nginx:1.13.9-alpine
 
-ENV NGINX_DIR /usr/share/nginx/html
-
-COPY --from=build-page /mazes/src/node_modules ${NGINX_DIR}/scripts
-COPY --from=build-page /mazes/src/static ${NGINX_DIR}
-#COPY --from=build-docs /mazes/docs/_site ${NGINX_DIR}/docs
+COPY public /usr/share/nginx/html
 
 EXPOSE 80
 
--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
-const port     = process.env.PORT || process.argv[2] || 80;
-const path     = require('path');
-const express  = require('express');
-const template = path.join(__dirname, 'static');
-const scripts  = path.join(__dirname, 'node_modules');
-const app      = express();
+const port     = process.env.PORT || process.argv[2] || 80
+const path     = require('path')
+const express  = require('express')
+const template = path.join(__dirname, '..', 'public')
+const app      = express()
 
-//------------------------------------------------------------------------------
-
-// Serve static site, and node_module dependencies
-app.use("/", express.static(template));
-app.use("/scripts", express.static(scripts));
-
-//------------------------------------------------------------------------------
-
-// Serve on specified port
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use("/", express.static(template))
+app.listen(port, () => console.log(`Listening on port ${port}`))
